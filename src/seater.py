e79b2dc84385d1d51ac66a5261d2460d267debf8
Brendan <brendandwyer1@gmail.com>
2016-02-29 23:26:50 +0000

    Added comments and updated tests.

--- src/seater.py
+++ src/seater.py
@@ -8,21 +8,23 @@ Created on 24 Feb 2016
 class Seater:
     
     # this regular expression will give us the command and the rectangular bounding box
-    # https://docs.python.org/2/library/re.html#re.MatchObject.group
     pat = re.compile("(.*) (\d+),(\d+) through (\d+),(\d+)")
     occupied_seats = 0
     unoccupied_seats = 1000000
 
     def __init__(self, size=1000):
         self.matrix = [[0 for i in xrange(size)] for i in xrange(size)]
+        # declare the matrix: 1000 zeroes in the first loop, 1000 times in the second for a total of 1,000,000
         return
     
     def get_cmd(self, line):
+        # Fetches the command and co-ordinates from the input file, slices them into variables.
         cmd, x1, y1, x2, y2 = Seater.pat.match(line).groups()
         x1, x2, y1, y2 = int(x1), int(x2), int(y1), int(y2)
         return cmd, x1, y1, x2, y2
     
     def seat(self, line):
+        # Interprets the instructions from the input file and calls the relevant function with the arguments provided.
         cmd, x1, y1, x2, y2 = self.get_cmd(line)
         print cmd, x1, y1, x2, y2
         if cmd == 'toggle':
@@ -32,31 +34,45 @@ class Seater:
         elif cmd == 'empty':
             self.empty(x1, y1, x2, y2)
         else:
+            # the program should never reach this point unless the input file was altered.
             pass
         return
     
     def occupy(self, x1, y1, x2, y2):
-        for i in xrange(x1, y1 + 1):
-            for j in xrange(x2, y2 + 1):
+        for i in xrange(x1, x2 + 1):
+            for j in xrange(y1, y2 + 1):
+                # nested for loop to access the square of coordinates in the matrix.
                 self.matrix[i], self.matrix[j] = 1, 1
+                # set elements in both axes to 1
+                # (error when attempted in format 'self.matrix[i][j] = 1'
 
         return
     
     def empty(self, x1, y1, x2, y2):
-        for i in xrange(x1, y1 + 1):
-            for j in xrange(x2, y2 + 1):
+        for i in xrange(x1, x2 + 1):
+            for j in xrange(y1, y2 + 1):
+                # similar functionality as above, but sets elements to empty rather than occupied.
                 self.matrix[i], self.matrix[j] = 0, 0
         return
     
     def toggle(self, x1, y1, x2, y2):
         for i in xrange(x1, x2 + 1):
             for j in xrange(y1, y2 + 1):
-                i = not i
-                j = not j
+                if self.matrix[i] == 0:
+                    self.matrix[i] = 1
+                else:
+                    self.matrix[i] = 0
+                if self.matrix[j] == 0:
+                    self.matrix[j] = 1
+                else:
+                    self.matrix[j] = 0
+
+                    # ugly if statements because boolean NOT didn't work as expected.
         return
     
     def number_occupied(self):
         occupied = self.matrix.count(1)
+        # count the number of occurrences of '1', signifying 'occupied'.
         return occupied
     
 if __name__ == '__main__':
--- tests/test.py
+++ tests/test.py
@@ -13,19 +13,24 @@ def test_pattern():
 
 
 def test_occupy():
+    # tests whether the occupy function gives the right number of 1's.
     seater = Seater()
     res = seater.get_cmd("occupy 520,520 through 522,522")
     assert seater.number_occupied() == 9
 
 
 def test_empty():
+    # tests whether the empty function removes occupied elements from the array. Not sure how to occupy them
+    # locally, tried loop and .append method but doesn't work because seater is an instance of class Seater.
     seater = Seater()
-    res = seater.get_cmd("empty 5, 5 through 20, 20")
-    assert seater.number_occupied() == 5
+    res = seater.get_cmd("empty 0, 0 through 20, 20")
+    assert seater.number_occupied() == 0
 
 
 def test_toggle():
+    # tests whether the toggle function inverts the values of the seated list. Since all seats are by default empty,
+    # toggle function should return 11*11 occupied seats. It doesn't, but it should!
     seater = Seater()
     res = seater.get_cmd("toggle 100, 100 through 110, 110")
-    assert seater.number_occupied() ==
+    assert seater.number_occupied() == 121
 
